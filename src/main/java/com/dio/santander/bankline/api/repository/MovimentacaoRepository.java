58398bc7b26a0dcce3e797a53b96236bc7df510d
Rafael <rafaelferreira.0096@gmail.com>
2022-05-05 14:19:28 -0300

    Implementação da busca por ID em movimentações

--- src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -1,5 +1,7 @@
 package com.dio.santander.bankline.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.dio.santander.bankline.api.model.Movimentacao;
 
 @Repository
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
-
+	
+	public List<Movimentacao>findByIdConta(Integer idConta);
+	
 }
